9a06e42787c18f5ca457b09d1abbc4ba6690bf24
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-07-19 18:13:19 +0700

    array of albums from fetch

--- lib/main.dart
+++ lib/main.dart
@@ -44,19 +44,20 @@ class AlbumContainer extends StatefulWidget {
   _AlbumState createState() => _AlbumState();
 }
 
-Future<Album> fetchAlbum() async {
-  final response = await http
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+Future<List<Album>> fetchAlbum() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
 
   if (response.statusCode == 200) {
-    return Album.fromJson(jsonDecode(response.body));
+    List responseJson = jsonDecode(response.body);
+    return responseJson.map((a) => Album.fromJson(a)).toList();
   } else {
     throw Exception('Failed');
   }
 }
 
 class _AlbumState extends State<AlbumContainer> {
-  late Future<Album> futureAlbum;
+  late Future<List<Album>> futureAlbum;
 
   @override
   void initState() {
@@ -64,17 +65,36 @@ class _AlbumState extends State<AlbumContainer> {
     futureAlbum = fetchAlbum();
   }
 
+  Widget _buildRow(Album entry) {
+    return ListTile(
+        title: Row(children: [
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(child: Text(entry.title)),
+            Text("${entry.id}", style: TextStyle(color: Colors.grey[500]))
+          ],
+        ),
+      )
+    ]));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Album')),
       body: Center(
-        child: FutureBuilder<Album>(
+        child: FutureBuilder<List<Album>>(
           future: futureAlbum,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return Text(
-                  "title: ${snapshot.data!.title}, id: ${snapshot.data!.id}");
+              return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  padding: const EdgeInsets.all(16),
+                  itemBuilder: (BuildContext context, int i) {
+                    return _buildRow(snapshot.data![i]);
+                  });
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
             }
